472cec8bf7da7fcdde2c8751860374f1f1fd5cb2
ayush-mahore <ayush.mahore.jobs@gmail.com>
2024-07-23 18:28:26 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [React.createElement(
+        "div",
+        { id: "child1" },
+        [React.createElement("h1", { id: "heading" }, "Hello --> C1"),
+        React.createElement("h2", { id: "heading2" }, "Hello2 --> C1"),
+        ]
+    ),
+    React.createElement(
+        "div",
+        { id: "child2" },
+        [React.createElement("h1", { id: "heading" }, "Hello --> C2"),
+        React.createElement("h2", { id: "heading2" }, "Hello2 --> C2"),
+        ]
+    ),
+    ]
+);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
